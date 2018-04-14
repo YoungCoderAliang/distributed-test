c7a0eafdbedc81a82a64261dc937aaec371b3d66
lizeliang <lizeliang@192.168.1.122>
2018-04-14 22:07:55 +0800

    java 10 测试

---
+++ distributed-test-zookeeper/src/main/java/distributed/test/zookeeper/java10/Java10Test.java
@@ -0,0 +1,9 @@
+package distributed.test.zookeeper.java10;
+
+import java.util.LinkedList;
+public class Java10Test {
+	public static void main(String[] args) {
+//		var str = new String("abc");
+//		str = new LinkedList<String>();
+	}
+}
